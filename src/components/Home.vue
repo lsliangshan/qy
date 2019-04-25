6ff2c5a86c800a518b9882303d2c5eef017b100d
梁山 <liangshan@192.168.0.106>
2019-04-26 01:21:44 +0800

    greeting

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,44 +1,46 @@
 <template>
   <div class="home_container">
-    <view-box body-padding-bottom="53px">
+    <!-- <view-box body-padding-bottom="53px">
       <ls-scroll class="main_scroll_container" :options="scrollOptions">
         <router-view name="ContentRouter"></router-view>
       </ls-scroll>
       <div class="bottom_tabbars_container" slot="bottom">
         <rqq-tabbars></rqq-tabbars>
       </div>
-    </view-box>
+    </view-box> -->
+    <greeting></greeting>
   </div>
 </template>
 <style scoped>
-  .home_container {
-    width: 100%;
-    height: 100%;
-  }
-  .main_scroll_container {
-    width: 100%;
-    height: 100%;
-  }
-  .bottom_tabbars_container {
-    width: 100%;
-    height: 53px;
-  }
+.home_container {
+  width: 100%;
+  height: 100%;
+}
+.main_scroll_container {
+  width: 100%;
+  height: 100%;
+}
+.bottom_tabbars_container {
+  width: 100%;
+  height: 53px;
+}
 </style>
 <script>
-  import { ViewBox } from 'vux'
-  export default {
-    name: 'Home',
-    data () {
-      return {
-        scrollOptions: {
-          scrollbar: false
-        }
+import { ViewBox } from 'vux'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      scrollOptions: {
+        scrollbar: false
       }
-    },
-    components: {
-      RqqTabbars: () => import('./RqqTabbars.vue'),
-      LsScroll: () => import('./plugins/LsScroll/LsScroll.vue'),
-      ViewBox
     }
+  },
+  components: {
+    Greeting: () => import('./Greeting.vue'),
+    RqqTabbars: () => import('./RqqTabbars.vue'),
+    LsScroll: () => import('./plugins/LsScroll/LsScroll.vue'),
+    ViewBox
   }
+}
 </script>
